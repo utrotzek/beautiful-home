f482c7644404f965e2952467f226cdb63b399d89
Uwe Trotzek <uwe.trotzek@gmail.com>
2019-02-20 23:32:28 +0100

    [FEATURE] implement home view

--- app/resources/views/Home.vue
+++ app/resources/views/Home.vue
@@ -1,8 +1,21 @@
 <template>
-    <div class="col-md-12">
-        Hier ist die Heimat
+    <div class="mb-0 text-center">
+        <div class="profile-pic">
+            <img src="../images/profile.png" class="img-fluid"/>
+        </div>
+        <div class="description">
+            <h1 class="text-uppercase mb-0">Hallo {{ userName }}</h1>
+            <hr class="star-dark">
+            <h2 class="font-weight-light mb-0">Viel Spaß beim Arbeiten und einen schönen Tag</h2>
+        </div>
     </div>
 </template>
 <script>
-    export default {}
+    export default {
+       data: function (){
+           return {
+               userName: "Vanessa"
+           }
+       }
+    }
 </script>
