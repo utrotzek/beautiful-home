a6e1c5042e97f08b44f397631fa4e3b0c7f0c423
Uwe Trotzek <uwe.trotzek@gmail.com>
2019-02-20 22:27:17 +0100

    [FEATURE] reimplement scroll to top feature

--- app/resources/js/components/ScrollToTop.vue
+++ app/resources/js/components/ScrollToTop.vue
@@ -3,3 +3,30 @@
         <i class="fas fa-angle-up"></i>
     </a>
 </template>
+
+<script>
+    export default {
+        methods: {
+            displayScrollToTop: function(event){
+                var scrollDistance = $(window).scrollTop();
+                console.log(scrollDistance);
+                if (scrollDistance > 300) {
+                    $('.scroll-to-top').fadeIn();
+                } else {
+                    $('.scroll-to-top').fadeOut();
+                }
+            }
+        },
+        data () {
+            return {
+                scrolled: false
+            };
+        },
+        beforeMount () {
+            window.addEventListener('scroll', this.displayScrollToTop);
+        },
+        beforeDestroy () {
+            window.removeEventListener('scroll', this.displayScrollToTop);
+        }
+    }
+</script>
