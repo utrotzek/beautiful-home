01e21d477b7c0e3a2b79d9884d90df9efbb3312d
Uwe Trotzek <uwe.trotzek@gmail.com>
2019-02-21 20:49:00 +0100

    [FEATURE] make profile pic responsive

--- app/resources/views/Home.vue
+++ app/resources/views/Home.vue
@@ -1,12 +1,21 @@
 <template>
-    <div class="mb-0 text-center">
-        <div class="profile-pic">
-            <img src="../images/profile.png" class="img-fluid"/>
-        </div>
-        <div class="description">
-            <h1 class="text-uppercase mb-0">Hallo {{ userName }}</h1>
-            <hr class="star-dark">
-            <h2 class="font-weight-light mb-0">Viel Spaß beim Arbeiten und einen schönen Tag</h2>
+    <div>
+        <div class="row justify-content-center align-self-center text-center">
+            <div class="col-12">
+                <h1>Willkommen</h1>
+            </div>
+            <div class="col-12 profile-pic">
+                <picture>
+                    <source media="(max-width: 576px)" srcset="../images/profile_100x100.png">
+                    <source media="(min-width: 800px)" srcset="../images/profile_256_256.png">
+                    <img src="../images/profile_256_256.png" class="img-fluid"/>
+                </picture>
+            </div>
+            <div class="col-12 description">
+                <h1 class="text-uppercase mb-0">Hallo {{ userName }}</h1>
+                <hr class="star-dark">
+                <h2 class="font-weight-light mb-0">{{ greetings[Math.floor(Math.random() * greetings.length)] }}</h2>
+            </div>
         </div>
     </div>
 </template>
@@ -14,7 +23,12 @@
     export default {
        data: function (){
            return {
-               userName: "Vanessa"
+               userName: "Vanessa",
+               greetings: [
+                   "Einen großartigen Tag!",
+                   "Viel Spaß beim Arbeiten und einen schönen Tag!",
+                   "Genieße den Tag!"
+               ]
            }
        }
     }
