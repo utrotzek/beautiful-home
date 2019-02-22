a2a738bb18308e8d869f601b74ddf5f6b1cd3fa2
Uwe Trotzek <uwe.trotzek@gmail.com>
2019-02-22 11:16:13 +0100

    [CHANGE] minor adjustments in recipe view

--- app/resources/views/Recipe.vue
+++ app/resources/views/Recipe.vue
@@ -2,22 +2,24 @@
     <div>
         <Headline text="Was möchtest Du tun?"></Headline>
 
-        <div id="button-menu">
-            <div class="row justify-content-md-center">
-                <div class="col-md-6 col-lg-4">
-                    <router-link to="/recipe/search" tag="button" class="btn btn-primary btn-block text-left">
-                        <span class="fa fa-search"></span>
-                        <span class="left-spacing">Rezepte suchen</span>
-                    </router-link>
-                    <router-link to="/recipe/edit" tag="button" class="btn btn-default btn-block text-left">
-                        <span class="fa fa-edit"></span>
-                        <span class="left-spacing">Ein neues Rezept eingeben</span>
-                    </router-link>
-                    <router-link to="/recipe/rate" tag="button" class="btn btn-default btn-block text-left">
-                        <i class="fa fa-bell"></i>
-                        <span class="badge badge-danger">{{ recipeRatingsCount }}</span>
-                        <span class="left-spacing">Gerichte bewerten</span>
-                    </router-link>
+        <div class="main-content">
+            <div id="button-menu">
+                <div class="row justify-content-md-center">
+                    <div class="col-md-6 col-lg-4">
+                        <router-link to="/recipe/search" tag="button" class="btn btn-primary btn-block text-left">
+                            <span class="fa fa-search"></span>
+                            <span class="left-spacing">Rezepte suchen</span>
+                        </router-link>
+                        <router-link to="/recipe/edit" tag="button" class="btn btn-default btn-block text-left mt-3">
+                            <span class="fa fa-edit"></span>
+                            <span class="left-spacing">Ein neues Rezept eingeben</span>
+                        </router-link>
+                        <router-link to="/recipe/rate" tag="button" class="btn btn-default btn-block text-left mt-3">
+                            <i class="fa fa-bell"></i>
+                            <span class="badge badge-danger">{{ recipeRatingsCount }}</span>
+                            <span class="left-spacing">Gerichte bewerten</span>
+                        </router-link>
+                    </div>
                 </div>
             </div>
         </div>
