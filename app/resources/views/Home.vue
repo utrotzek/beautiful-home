4f0ba826ce834de0a047b609ff23c7cb675f8f30
Uwe Trotzek <uwe.trotzek@gmail.com>
2019-02-23 22:40:27 +0100

    [FEATURE] implement daytime  dependent welcome message

--- app/resources/views/Home.vue
+++ app/resources/views/Home.vue
@@ -12,8 +12,12 @@
                 </picture>
             </div>
             <div class="col-12 description">
-                <h1 class="text-uppercase mb-0">Hallo {{ userName }}</h1>
+                <!-- daytime depending welcome message -->
+                <h1 v-if="isMorning()" class="text-uppercase mb-0">Guten Morgen!</h1>
+                <h1 v-else-if="isAfternon()" class="text-uppercase mb-0">Guten Tag!</h1>
+                <h1 v-else-if="isEvening()" class="text-uppercase mb-0">Guten Abend!</h1>
                 <hr class="star-dark">
+                <!-- random greetings -->
                 <h2 class="font-weight-light mb-0">{{ greetings[Math.floor(Math.random() * greetings.length)] }}</h2>
             </div>
         </div>
@@ -21,15 +25,31 @@
 </template>
 <script>
     export default {
-       data: function (){
+       data (){
            return {
                userName: "Vanessa",
                greetings: [
                    "Einen großartigen Tag!",
                    "Viel Spaß beim Arbeiten und einen schönen Tag!",
                    "Genieße den Tag!"
-               ]
+               ],
            }
-       }
+       },
+        computed: {
+            hours () {
+                return new Date().getHours();
+            }
+        },
+        methods: {
+            isMorning () {
+                return this.hours > 4 && this.hours < 11;
+            },
+            isAfternon () {
+                return this.hours >= 11 && this.hours < 18;
+            },
+            isEvening () {
+                return this.hours >= 18 || this.hours <= 4;
+            }
+        }
     }
 </script>
