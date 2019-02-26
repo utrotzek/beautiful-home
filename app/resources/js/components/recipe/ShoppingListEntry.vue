56545ac037442581fa98f4bd0dd76347c73ea430
Uwe Trotzek <uwe.trotzek@gmail.com>
2019-02-26 19:10:03 +0100

    [TASK] remove unused DOM elements

--- app/resources/js/components/recipe/ShoppingListEntry.vue
+++ app/resources/js/components/recipe/ShoppingListEntry.vue
@@ -10,52 +10,13 @@
         </button>
 
         <div class="preview d-inline" v-if="!editMode">
-            <!-- count edit -->
-            <label :for="'countEdit' + shoppingListItem.id" class="sr-only">Anzahl</label>
-            <input
-                    :id="'countEdit' + shoppingListItem.id"
-                    class="small countInput"
-                    title="Anzahl bearbeiten"
-                    v-if="editMode"
-                    v-model="shoppingListItem.item.count"
-                    @keyup.enter="deactivateEditMode"
-                    v-focus/>
-            <span v-else>
-            {{shoppingListItem.item.count}}
-        </span>
+            <span>
+                {{shoppingListItem.item.count}}
+            </span>
 
             <!-- unit inline edit -->
-            <select
-                    v-model="selectedUnit"
-                    v-if="editMode"
-                    title="Einheit auswählen"
-            >
-                <option v-for="unit in allUnits" v-bind:value="unit.id">
-                    {{unit.title + ' (' + unit.abbreviation + ')'}}
-                </option>
-            </select>
-            <span v-else>{{shoppingListItem.item.unit.abbreviation}}</span>
-
-            <label class="sr-only" :for="'titleEdit' + shoppingListItem.id">Name</label>
-            <input
-                    :id="'titleEdit' + shoppingListItem.id"
-                    title="Titel bearbeiten"
-                    v-if="editMode"
-                    type="text"
-                    class="small"
-                    @keyup.enter="deactivateEditMode"
-                    v-model="shoppingListItem.item.title"
-            />
-            <span v-else>
-            {{shoppingListItem.item.article.title}}
-        </span>
-
-            <button
-                    v-if="editMode"
-                    @click="deactivateEditMode()"
-                    class="btn btn-secondary small btn-sm">
-                OK
-            </button>
+            <span>{{shoppingListItem.item.unit.abbreviation}}</span>
+            <span>{{shoppingListItem.item.article.title}}</span>
         </div>
 
         <div class="editor d-inline" v-if="editMode">
@@ -102,7 +63,7 @@
             },
             updateItem(updatedItem) {
                 this.shoppingListItem.item = updatedItem;
-                this.toggleEditMode();
+                this.toggleEditMode;
                 console.log(updatedItem);
             }
         }
