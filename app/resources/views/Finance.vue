89607e77920796580d9a8780db533bc4d25e83a0
Uwe Trotzek <uwe.trotzek@gmail.com>
2020-04-07 23:39:33 +0200

    [FEATURE] implement "accounting/templates" toggle

--- app/resources/views/Finance.vue
+++ app/resources/views/Finance.vue
@@ -5,86 +5,119 @@
             ref="topProgress"
             color="#f8f9fa"
         />
-        <div class="main-content">
-            <div class="row text-center">
-                <div class="col">
-                    <year
-                        :year="year"
-                        @update-year="updateYear"
-                    />
+        <div id="main-content">
+            <div class="row justify-content-center">
+                <div class="col-xs-8 col-md-3  mt-4 mb-3">
+                    <ul class="nav nav-tabs">
+                        <li class="nav-item">
+                            <a
+                                class="nav-link"
+                                :class="{active: view==='accounting'}"
+                                href="#"
+                                @click="showAccounting"
+                            >Buchhaltung</a>
+                        </li>
+                        <li class="nav-item">
+                            <a
+                                class="nav-link"
+                                :class="{active: view==='templates'}"
+                                href="#"
+                                @click="showTemplates"
+                            >Vorlagen</a>
+                        </li>
+                    </ul>
                 </div>
             </div>
-            <!-- TODO make layout nicer -->
             <div
-                v-if="errorMessage"
-                class="text-center"
+                v-if="view==='accounting'"
+                id="accountingView"
             >
-                <div
-                    class="alert alert-danger"
-                    role="alert"
-                >
-                    {{ errorMessage }}
-                </div>
-            </div>
-            <div
-                v-if="loaded"
-                class="row justify-content-center"
-            >
-                <div
-                    v-if="periods.length"
-                    class="col-3 mt-4"
-                >
-                    <div class="list-group">
-                        <router-link
-                            v-for="period in periods"
-                            :key="period.id"
-                            class="list-group-item list-group-item-action"
-                            :to="{name: 'accounting', params: {periodId: period.id}}"
-                        >
-                            <span
-                                v-if="period.completed"
-                                class="fas fa-lock"
-                            ></span>
-                            {{ period.monthName }}
-                        </router-link>
-
-                        <a
-                            v-if="nextMonth <= 12"
-                            class="list-group-item list-group-item-action active"
-                            href="#"
-                            @click="createPeriod(nextMonth)"
-                        >
-                            <span class="fa fa-plus-circle"></span>
-                            {{ nextMonthName }}
-                        </a>
+                <div class="row text-center">
+                    <div class="col">
+                        <year
+                            :year="year"
+                            @update-year="updateYear"
+                        />
                     </div>
                 </div>
+                <!-- TODO make layout nicer -->
                 <div
-                    v-else
-                    class="col-3 mt-4 mt-4 pl-5"
+                    v-if="errorMessage"
+                    class="text-center"
                 >
                     <div
-                        class="alert alert-info"
+                        class="alert alert-danger"
                         role="alert"
                     >
-                        Für dieses Jahr liegen keine Daten vor
-                    </div>
-
-                    <div
-                        v-if="nextMonth <= 12"
-                        class="list-group"
-                    >
-                        <a
-                            v-if="nextMonth <= 12"
-                            class="list-group-item list-group-item-action active"
-                            href="#"
-                            @click="createPeriod(nextMonth)"
-                        >
-                            <span class="fa fa-plus-circle"></span>
-                            {{ nextMonthName }}
-                        </a>
+                        {{ errorMessage }}
                     </div>
                 </div>
+                <div
+                    v-if="loaded"
+                    class="row justify-content-center"
+                >
+                    <div
+                        v-if="periods.length"
+                        class="col-xs-8 col-md-3 mt-4"
+                    >
+                        <div class="list-group">
+                            <router-link
+                                v-for="period in periods"
+                                :key="period.id"
+                                class="list-group-item list-group-item-action"
+                                :to="{name: 'accounting', params: {periodId: period.id}}"
+                            >
+                                <span
+                                    v-if="period.completed"
+                                    class="fas fa-lock"
+                                ></span>
+                                {{ period.monthName }}
+                            </router-link>
+
+                            <a
+                                v-if="nextMonth <= 12"
+                                class="list-group-item list-group-item-action active"
+                                href="#"
+                                @click="createPeriod(nextMonth)"
+                            >
+                                <span class="fa fa-plus-circle"></span>
+                                {{ nextMonthName }}
+                            </a>
+                        </div>
+                    </div>
+                    <div
+                        v-else
+                        class="col-3 mt-4 mt-4 pl-5"
+                    >
+                        <div
+                            class="alert alert-info"
+                            role="alert"
+                        >
+                            Für dieses Jahr liegen keine Daten vor
+                        </div>
+
+                        <div
+                            v-if="nextMonth <= 12"
+                            class="list-group"
+                        >
+                            <a
+                                v-if="nextMonth <= 12"
+                                class="list-group-item list-group-item-action active"
+                                href="#"
+                                @click="createPeriod(nextMonth)"
+                            >
+                                <span class="fa fa-plus-circle"></span>
+                                {{ nextMonthName }}
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div
+                v-if="view==='templates'"
+                id="templateView"
+            >
+                Templates...
             </div>
         </div>
     </div>
@@ -104,7 +137,8 @@ export default {
             allMonths: [],
             periods: [],
             loaded: false,
-            errorMessage: ""
+            errorMessage: "",
+            view: "accounting"
         };
     },
     computed: {
@@ -167,6 +201,12 @@ export default {
             await monthPromise;
             this.$refs.topProgress.done();
             this.loaded = true;
+        },
+        showAccounting() {
+            this.view = "accounting";
+        },
+        showTemplates() {
+            this.view = "templates";
         }
     },
 };
