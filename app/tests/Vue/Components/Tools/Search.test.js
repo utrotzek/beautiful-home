47703b47965e6c14d4867e214288dee02a1da820
Uwe Trotzek <uwe.trotzek@gmail.com>
2020-04-14 20:39:26 +0200

    [TASK] add test for search component

---
+++ app/tests/Vue/Components/Tools/Search.test.js
@@ -0,0 +1,41 @@
+import {mount} from "@vue/test-utils";
+import Vue from "vue";
+import Search from "../../../../resources/js/components/tools/Search";
+
+describe("Overview", () => {
+    let wrapper = null;
+
+    beforeEach(() => {
+        wrapper = mount(Search,{
+            propsData: {
+            }
+        });
+    });
+
+    it ("renders properly", () => {
+        expect(wrapper.element).toMatchSnapshot();
+    });
+
+    it ("fires event if query field changes", () =>  {
+        const expectedTerm = "My search term";
+
+        enterSearchTerm();
+        expect(wrapper.emitted().searched[0]).toEqual([expectedTerm]);
+    });
+
+    it ("can clear the search term", () =>{
+        const expectedSearchTerm = "";
+
+        enterSearchTerm();
+        wrapper.find(".search-clear").trigger("click");
+
+        expect(wrapper.emitted().searched[1]).toEqual([expectedSearchTerm]);
+
+    });
+
+    function enterSearchTerm() {
+        const searchTerm = "My search term";
+        wrapper.find(".search-control").setValue(searchTerm);
+        wrapper.find(".search-control").trigger("keyup");
+    }
+});
